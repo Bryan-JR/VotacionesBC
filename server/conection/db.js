a15e472c7099cfa39997bf264a9df01eeefd537b
Bryan-JR <95584508+Bryan-JR@users.noreply.github.com>
2023-11-17 11:40:27 -0500

    Arreglo de detalles

--- server/conection/db.js
+++ server/conection/db.js
@@ -1,11 +1,15 @@
-import mysql from "mysql"
-import dotenv from "dotenv"
-import nodemailer from "nodemailer"
+// Importación de módulos necesarios
+import mysql from "mysql";  // Importa el módulo de MySQL para la conexión a la base de datos
+import dotenv from "dotenv";  // Importa el módulo dotenv para cargar variables de entorno desde un archivo .env
+import nodemailer from "nodemailer";  // Importa el módulo nodemailer para enviar correos electrónicos
 
+// Configuración de variables de entorno usando dotenv
 dotenv.config({path: "../server/env/.env"});
 
+// Imprime el nombre de la base de datos desde las variables de entorno
 console.log("Base de datos: "+process.env.DB_NAME);
 
+// Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -13,6 +17,7 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+// Configuración del transporte de correo electrónico con nodemailer
 export const email = nodemailer.createTransport({
     service: process.env.EMAIL_SERVICE,
     auth: {
@@ -21,13 +26,17 @@ export const email = nodemailer.createTransport({
     }
 });
 
+// Conexión a la base de datos MySQL
 connection.connect((err) => {
     if (err) {
+        // Manejo de errores en caso de fallo en la conexión
         console.error("Error al conectar a la base de datos: "+ err.message);
         return;
     } else {
+        // Mensaje de éxito cuando la conexión es exitosa
         console.log("Conectado a la base de datos.");
     }
 });
 
+// Exporta la conexión a la base de datos para su uso en otros módulos
 export const db = connection;
